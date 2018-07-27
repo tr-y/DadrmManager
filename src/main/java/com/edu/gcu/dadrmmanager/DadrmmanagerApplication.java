03f99b6b3e7ca3df596a4a2cb7c861c40618f2fc
HelloWorld <756360729@qq.com>
2018-07-27 17:30:30 +0800

    基础部署

---
+++ src/main/java/com/edu/gcu/dadrmmanager/DadrmmanagerApplication.java
@@ -0,0 +1,12 @@
+package com.edu.gcu.dadrmmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadrmmanagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DadrmmanagerApplication.class, args);
+  }
+}
